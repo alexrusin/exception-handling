10fc77ab77c28a90ee7cb9e79069b3fa6715a99e
Alex Rusin <alexrusin_2000@yahoo.com>
2019-04-28 12:20:11 -0700

    config example

---
+++ config/config_example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'env' => 'production',
+    'mail_driver' => 'smtp',
+    'mail_host' => 'smtp.mailtrap.io',
+    'mail_port' => '2525',
+    'mail_username' => 'username',
+    'mail_password' => 'password',
+    'mail_from_address' => 'info@app.com',
+    'mail_to_admin' => 'admin@admin.com'
+];
