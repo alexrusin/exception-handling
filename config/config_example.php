a08494422aa88127806524b18a74dec78ba6a4dd
Alex Rusin <alex@sunriseintegration.com>
2019-04-29 08:46:23 -0700

    add mandrill mailer

--- config/config_example.php
+++ config/config_example.php
@@ -2,7 +2,9 @@
 
 return [
     'env' => 'production',
+    // values for mail_driver: smtp, mandrill, array
     'mail_driver' => 'smtp',
+    'mandrill_key' => 'mandrill-key',
     'mail_host' => 'smtp.mailtrap.io',
     'mail_port' => '2525',
     'mail_username' => 'username',
